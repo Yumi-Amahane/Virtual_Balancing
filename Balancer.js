ca32163779d5c3fd14f9e8317116367110c50ad0
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2021-05-10 23:20:31 +0900

    Update Balancer.js
    Add calculate method

--- Balancer.js
+++ Balancer.js
@@ -1,5 +1,22 @@
+
+var VBalance==VBalance||{};
+function shokika(){
+  VBalance.theta=0;//バランサー角度
+  VBalance.alpha=0;//ジャイロ角度
+  VBalance.omega=0;//バランサー速度
+  VBalance.A=5;//比例定数 $\frac{2mgy}{I}$
+}
+
+function calc(){
+  VBalance.omega-=A*sin(alpha+theta);
+  VBalance.theta+=VBalance.omega;
+  
+  document.getElementById("Status")=Status+"<br>omega="+omega+"<br>Arg="+VBalance. VBalance.alpha+VBalance.theta;
+}
+
+
 window.ondeviceorientation=function(event){
-	var alpha=event.alpha;
+	VBalance.alpha=event.alpha;
 	var beta=event.beta;
 	var gamma=event.gamma;
 	var Status="alpha="+alpha+"<br>beta"+beta+"<br>gamma="+gamma;
