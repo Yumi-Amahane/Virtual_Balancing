b598b8e0817dbf4b7ab1afdb11e4876d3e99f1f5
Rufina <feld_s_labo@yahoo.co.jp>
2021-05-11 13:19:52 +0900

    Add Gyro

--- Balancer.js
+++ Balancer.js
@@ -9,23 +9,47 @@ function shokika(){
 }
 
 function calc(){
-  VBalance.omega=VBalance.omega-VBalance.A*Math.sin((VBalance.alpha+VBalance.theta)/180*Math.PI);
+  VBalance.omega-=VBalance.A*Math.sin((VBalance.alpha+VBalance.theta)/180*Math.PI);
   VBalance.omega*=VBalance.Loss;
   VBalance.theta+=VBalance.omega;
   
   var log="<br>omega="+VBalance.omega+"<br>Arg="+VBalance.alpha+"<br>theta="+VBalance.theta;
   console.log(log);
   document.getElementById("Status").innerHTML=log;
+  
+  picrotation(arg);
 }
 
 function Start(){
-  VBalance.interval=setInterval(calc,50);
+	if(window.DeviceOrientationEvent){
+		if(DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission==='fuction'){
+			VBalance.bannar= '<div  style="z-index: 1; position: absolute; width: 100%; background-color: rgb(0, 0, 0);" onclick="ClickRequestDeviceSensor();" id="sensorrequest"><p style="color: rgb(0, 0, 255);">センサーの有効化</p></div>';
+    $('body').prepend( banner );
+  	}else{
+		window.addEventListener("deviceorientation",deviceOrientation);
+	}
+	VBalance.interval=setInterval(calc,50);
 }
 
-window.ondeviceorientation=function(event){
-	VBalance.alpha=event.alpha;
-	var beta=event.beta;
-	var gamma=event.gamma;
-	 VBalance.Status="alpha="+alpha+"<br>beta"+beta+"<br>gamma="+gamma;
-	document.getElementById("Status")=Status;
+function picrotation(arg){
+	
+}
+function deviceOrientation(event){
+	event.preventDeafult();
+	VBalance.alpha=-1*event.alpha;
+});
+
+
+function ClickRequestDeviceSensor(){
+  //. ユーザーに「許可」を求めるダイアログを表示
+  DeviceOrientationEvent.requestPermission().then( function( response ){
+    if( response === 'granted' ){
+      //. 許可された場合のみイベントハンドラを追加できる
+      window.addEventListener( "deviceorientation", deviceOrientation );
+      //. 画面上部のボタンを消す
+      $('#sensorrequest').css( 'display', 'none' );
+    }
+  }).catch( function( e ){
+    console.log( e );
+  });
 }
