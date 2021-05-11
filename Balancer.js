bce4678fd3c253b2bfc2d821e7dcf9a12217d439
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2021-05-11 11:28:44 +0900

    Update Balancer.js

--- Balancer.js
+++ Balancer.js
@@ -1,14 +1,16 @@
 
 var VBalance==VBalance||{};
 function shokika(){
-  VBalance.theta=0;//バランサー角度
+  VBalance.theta=10;//バランサー角度
   VBalance.alpha=0;//ジャイロ角度
   VBalance.omega=0;//バランサー速度
-  VBalance.A=5;//比例定数 $\frac{2mgy}{I}$
+  VBalance.A=0.01;//比例定数 $\frac{2mgy}{I}$
+  VBalance.Loss=0.999;//エネルギーのロス
 }
 
 function calc(){
   VBalance.omega-=A*sin(alpha+theta);
+  VBalance.omega*=VBalance.Loss;
   VBalance.theta+=VBalance.omega;
   
   document.getElementById("Status")=Status+"<br>omega="+omega+"<br>Arg="+VBalance. VBalance.alpha+VBalance.theta;
