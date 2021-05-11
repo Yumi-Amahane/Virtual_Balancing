da119504efe86438d4270309a22e23daad82e034
Rufina <feld_s_labo@yahoo.co.jp>
2021-05-11 14:11:25 +0900

    fix Permission

--- Balancer.js
+++ Balancer.js
@@ -26,7 +26,7 @@ function Start(){
 	if(window.DeviceOrientationEvent){
 		console.log("Event");
 		
-		if(DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission==='fuction'){
+		if(DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function'){
 			VBalance.bannar= '<div  style="z-index: 1; position: absolute; width: 100%; background-color: rgb(0, 0, 0);" onclick="ClickRequestDeviceSensor();" id="sensorrequest"><p style="color: rgb(0, 0, 255);">センサーの有効化</p></div>';
 			console.log("bannar");
 			
