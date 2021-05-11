85985351bef990b883a19c886973394213a0437a
Rufina <feld_s_labo@yahoo.co.jp>
2021-05-11 11:45:07 +0900

    Add Interval

--- Balancer.js
+++ Balancer.js
@@ -2,18 +2,18 @@
 var VBalance==VBalance||{};
 function shokika(){
   VBalance.theta=10;//Arg Balancer
-  VBalance.alpha=0;//Arg gyro
+  VBalance.alpha=10;//Arg gyro
   VBalance.omega=0;//Arg velocity
   VBalance.A=0.01;//Const $\frac{2mgy}{I}$
   VBalance.Loss=0.999;//Enegy loss const
 }
 
 function calc(){
-  VBalance.omega-=A*sin(alpha+theta);
+  VBalance.omega-=A*Math.sin(alpha+theta);
   VBalance.omega*=VBalance.Loss;
   VBalance.theta+=VBalance.omega;
   
-  document.getElementById("Status")=Status+"<br>omega="+omega+"<br>Arg="+VBalance. VBalance.alpha+VBalance.theta;
+  document.getElementById("Status")="<br>omega="+VBalance.omega+"<br>Arg="+VBalance.alpha+"<br>theta="+VBalance.theta;
 }
 
 VBalance.interval=setinterval(calc,5);
@@ -23,6 +23,6 @@ window.ondeviceorientation=function(event){
 	VBalance.alpha=event.alpha;
 	var beta=event.beta;
 	var gamma=event.gamma;
-	var Status="alpha="+alpha+"<br>beta"+beta+"<br>gamma="+gamma;
+	 VBalance.Status="alpha="+alpha+"<br>beta"+beta+"<br>gamma="+gamma;
 	document.getElementById("Status")=Status;
 }
